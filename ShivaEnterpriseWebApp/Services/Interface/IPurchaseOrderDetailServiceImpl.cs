32c3019afb556adc1e7dd7c423d460762095cd52
pooja soni <poojasoni7024@gmail.com>
2024-03-05 22:42:56 +0530

    purchase

---
+++ ShivaEnterpriseWebApp/Services/Interface/IPurchaseOrderDetailServiceImpl.cs
@@ -0,0 +1,13 @@
+﻿using ShivaEnterpriseWebApp.Model;
+
+namespace ShivaEnterpriseWebApp.Services.Interface
+{
+    public interface IPurchaseOrderDetailServiceImpl
+    {
+        Task<List<PurchaseOrderDetail>> GetPurchaseOrderDetailList(string authToken);
+        Task<PurchaseOrderDetail> GetPurchaseOrderDetailById(string purchaseorderdetailId, string authToken);
+        Task<(bool successs, string message)> DeletePurchaseOrderDetail(string purchaseorderdetailId, string authToken);
+        Task<(bool success, string message)> AddPurchaseOrderDetailDetailsAsync(PurchaseOrderDetail purchaseorderdetail, string authToken);
+        Task<(bool success, string message)> EditPurchaseOrderDetailDetailsAsync(PurchaseOrderDetail purchaseorderdetail, string authToken);
+    }
+}
